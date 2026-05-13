34d43f4b3242c552b1200a16ec8572a8465ba23d
berrypizza <kkh0510ks@gmail.com>
2026-05-13 15:01:07 +0900

    edit hidden

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
           </nav>
         </div>
 
-        <div className="hidden items-center gap-3 md:flex">
+        <div className=" items-center gap-3 flex">
           <a
             href="tel:010-9127-3024"
             className="rounded-xl bg-neutral-100 px-4 py-2 text-xs font-black text-neutral-800">
